89d1611a6eeca7a079d639b1cdfce48d8fc54d28
Tat-Obmetitsa <twinkle.lif.love@gmail.com>
2021-03-09 21:19:38 +0200

    init: add components

---
+++ src/components/App/App.js
@@ -0,0 +1,21 @@
+// import React, { Component } from 'react';
+// import s from '../App/App.module.css';
+// import Button from '../Button/Button';
+// import ImageGallery from '../ImageGallery/ImageGallery';
+// import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
+// import Loader from '../Loader/Loader';
+// import Modal from '../Modal/Modal';
+// import Searchbar from '../Searchbar/Searchbar';
+// import shortid from 'shortid';
+
+// class App extends Component  {
+//    state = {
+
+//    };
+//    render() {}
+//   return (
+
+//   );
+// }
+
+// export default App;
